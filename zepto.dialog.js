4640e4cf70869544aa387ddedb194a68a1a5d3c5
luk <lovemomoco@gmail.com>
2017-01-14 18:41:05 +0800

    update

--- zepto.dialog.js
+++ zepto.dialog.js
@@ -27,8 +27,48 @@
         } else {
             this.isConfig = true;
         }
+
+        //创建基本的DOM
+        this.body = $('body');
+        //创建遮罩层
+        this.mask = $('<div class="g-dialog-container">');
+        //创建弹出框
+        this.win = $('<div class="dialog-window">');
+        //创建弹出框头部
+        this.winHeader = $('<div class="dialog-header"></div>');
+        //创建提示信息
+        this.winContent = $('<div class="dialog-content">');
+        //创建弹出框按钮组
+        this.winFooter = $('<div class="dialog-footer">');
+
+        //渲染DOM
+        this.create();
+
     };
-    Dialog.prototype = {};
+    Dialog.prototype = {
+        //创建弹出框
+        create: function () {
+
+            var _this_ = this,
+                config = this.config,
+                mask = this.mask,
+                win = this.win,
+                header = this.winHeader,
+                content = this.winContent,
+                footer = this.winFooter,
+                body = this.body;
+
+            //如果没有传递任何配置参数
+            //就弹出一个等待的图标形式的弹框
+            if (this.isConfig) {
+                win.append(header.addClass('waiting'));
+                mask.append(win);
+                body.append(mask);
+            } else {
+
+            }
+        }
+    };
 
     window.Dialog = Dialog;
 
